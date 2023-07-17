d9a630bdcdaae56510e8103ad9a26bf3bf2f6567
Imtnk <133017410+Imtnk@users.noreply.github.com>
2023-07-17 20:58:56 +0700

    Update test.py

--- python/test.py
+++ python/test.py
@@ -1,6 +1,16 @@
 import time
 from datetime import datetime
-from buildhat import Motor
+from buildhat import Motor, Hat
+
+def until_finds_hat(t):
+    try:
+        return Hat()
+    except:
+        if(t>0):
+            time.sleep(1)
+            return until_finds_hat(t-1)
+        else:
+            return False
 
 # saves logs to cronlog in system
 now = datetime.now()
@@ -8,11 +18,14 @@ current_time = now.strftime("%H:%M:%S")
 print("Hello")
 print("Latest login =",current_time)
 
-# basic motor function for visual proof that program is actually run on startup
-motorA = Motor('A')
-motorB = Motor('B')
-motorA.set_default_speed(-100)
-motorB.set_default_speed(100)
-motorA.run_for_seconds(1, blocking=False)
-motorB.run_for_seconds(1, blocking=False)
-time.sleep(3)
+# recursive function to wait for hat to be connected
+if until_finds_hat(30) is not None: # set maximum find time to 30 seconds
+    # basic motor function for visual proof that program is actually run on startup
+    time.sleep(10)
+    motorA = Motor('A')
+    motorB = Motor('B')
+    motorA.set_default_speed(-100)
+    motorB.set_default_speed(100)
+    motorA.run_for_seconds(1, blocking=False)
+    motorB.run_for_seconds(1, blocking=False)
+    time.sleep(3)
